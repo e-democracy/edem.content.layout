34a50b2f5bd631870252c9c3c91716fb1d162ba1
William Bushey <wbushey@gmail.com>
2013-11-22 14:30:47 -0600

    Declared SiteNavMM Viewlet Manager Implemented sitenav menu in titlebar Pulling in Google's Open Sans font

---
+++ edem/content/layout/interfaces.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+from zope.viewlet.interfaces import IViewletManager
+
+
+class IEDemSiteNavMMVM(IViewletManager):
+    '''Viewlet Manager for E-Democracy's site navigation mega menu'''
