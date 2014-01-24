9c0cf4324fa13727f9cb7c024c9b6fd951e23f95
William Bushey <wbushey@gmail.com>
2014-01-24 14:03:31 -0600

    Created a contentprovider to list joinable, member, and suggested groups in a nav menu Created supporting templates Calling member and suggested groups contentprovider in site menu

---
+++ edem/content/layout/contentprovider.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from zope.component import createObject
+from zope.cachedescriptors.property import Lazy
+import AccessControl
+from zope.contentprovider.interfaces import UpdateNotCalled
+from zope.pagetemplate.pagetemplatefile import PageTemplateFile
+from gs.viewlet.contentprovider import SiteContentProvider
+from Products.GSGroup.interfaces import IGSGroupInfo
+
+from logging import getLogger
+log = getLogger('edem.groups.contentprovider')
+
+
+class NavGroupsListingContentProvider(SiteContentProvider):
+
+    def __init__(self, context, request, view):
+        super(NavGroupsListingContentProvider,
+              self). __init__(context, request, view)
+        self.pageTemplateFileName = "browser/templates/contentProviders/"
+        self.groupsInfo = createObject('groupserver.GroupsInfo', context)
+        self.updated = False
+
+    def update(self):
+        if self.maxGroupsToDisplay < 0:
+            raise ValueError('maxGroupsToDisplay must be 0 or greater')
+        self.pageTemplateFileName += self.groupsTypeToDisplay + '.pt'
+        self.pageTemplate = PageTemplateFile(self.pageTemplateFileName)
+        self.updated = True
+
+    def render(self):
+        if not self.updated:
+            raise UpdateNotCalled
+        return self.pageTemplate(view=self)
+
+    @Lazy
+    def joinable_groups(self):
+        u = self.loggedInUser.user
+        joinableGroups = self.groupsInfo.get_joinable_groups_for_user(u)
+        if self.maxGroupsToDisplay:
+            joinableGroups = joinableGroups[:self.maxGroupsToDisplay]
+        groups = map(IGSGroupInfo, joinableGroups)
+        return groups
+
+    @Lazy
+    def member_groups(self):
+        user = AccessControl.getSecurityManager().getUser()
+        memberGroups = self.groupsInfo.get_member_groups_for_user(user, user)
+        if self.maxGroupsToDisplay:
+            memberGroups = memberGroups[:self.maxGroupsToDisplay]
+        groups = map(IGSGroupInfo, memberGroups)
+        return groups
+
+    @Lazy
+    def suggested_groups(self):
+        groupNames = self.context.getProperty('suggestedGroups', [])
+        suggestedGroups = []
+        for groupName in groupNames:
+            suggestedGroups.append(
+                createObject('groupserver.GroupInfo', self.context, groupName))
+        return suggestedGroups
+            
+            
--- edem/content/layout/interfaces.py
+++ edem/content/layout/interfaces.py
@@ -1,6 +1,35 @@
 # coding=utf-8
+from __future__ import unicode_literals
 from zope.viewlet.interfaces import IViewletManager
+from zope.contentprovider.interfaces import IContentProvider
+from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema import ASCIILine, Choice, Int
 
 
 class IEDemSiteNavMMVM(IViewletManager):
     '''Viewlet Manager for E-Democracy's site navigation mega menu'''
+
+class IGSNavGroupsListing(IContentProvider):
+    '''Content Provider for a listing of groups in a nav menu'''
+    groupsTypeToDisplay = Choice(
+        title="Type of Groups to Display",
+        description="Defines which groups to display to the user: "
+                    "groups that the user can join (joinableGroups)(default), "
+                    "groups that the user is a member of (memberOfGroups), or "
+                    "suggested groups for the user to join (suggestedGroups).",
+        vocabulary=SimpleVocabulary.fromValues([
+                    'joinableGroups',
+                    'memberOfGroups',
+                    'suggestedGroups']),
+        required=False,
+        default='joinableGroups'
+
+    )
+
+    maxGroupsToDisplay = Int(
+        title="Maximum Groups to Display",
+        description="The maximum number of groups to display to the user."
+                    "A value of 0 (the default) results in all groups being "
+                    "displayed. Negative values will throw an error.",
+        required=False,
+        default=0)
